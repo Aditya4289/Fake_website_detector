a4e2050176960965191b99acd41640232e6ce426
Aditya Pareek <123439444+Aditya4289@users.noreply.github.com>
2024-01-18 09:14:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function performScan() {
+    var url = document.getElementById("url-input").value;
+    
+    // Add your scan logic here
+
+    // For demo purposes, let's show an alert
+    alert("Scanning: " + url);
+}
